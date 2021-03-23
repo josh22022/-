f94edffe458b6a7c95715d59b61f9885025621d8
josh22022 <52907789+josh22022@users.noreply.github.com>
2021-03-23 20:45:32 +0800

    Update 螺旋矩陣.py

--- 螺旋矩陣/螺旋矩陣.py
+++ 螺旋矩陣/螺旋矩陣.py
@@ -3,11 +3,11 @@ Case : 螺旋矩陣
 ID : 407180221
 """
 import numpy as np
-n=eval(input('Input the size of the matrix (odd number):')) #奇方陣的階數
+n=eval(input("Input the size of the matrix (odd number):")) 
 if (n%2) == 0:
     print("Error ! Not an odd number.")
 else :
-    a = np.random.randint(10, size=(n, n)) #隨機的奇方陣,element < 10
+    a = np.random.randint(10, size=(n, n)) 
     print("===============")
     print("Random matrix :\n",a)
     
@@ -31,9 +31,13 @@ else :
     print("Final :","[ 1 , 1 ] =",a[0,0])
 
 '''
+說明：
+    隨機產生奇方陣，以中心點順時鐘及逆時鐘向外旋轉，並回報路徑上的每個元素值。
 功能：
     判斷是否為奇數
     創建隨機方陣
     順時鐘螺旋
     逆時鐘螺旋
+思路：
+    與中心點的相對位置具規律性，使用迴圈將每層四角取出。
 '''
